51c04d84ddff3d49f374dfc979e1ad20a76ae9c7
bobo <micro_sobby@outlook.com>
2023-12-12 21:31:54 +0800

    remove all local templates

--- src/fabritor/UI/panel/TemplatePanel/template-list.ts
+++ src/fabritor/UI/panel/TemplatePanel/template-list.ts
@@ -1,14 +1,1 @@
-export default [
-  {
-    key: 'fans',
-    template: require('@/assets/templates/fans.json'),
-    desc: '涨粉小技巧',
-    preview: require('@/assets/templates/fans.png')
-  },
-  {
-    key: 'post',
-    template: require('@/assets/templates/post.json'),
-    desc: '社群福利',
-    preview: require('@/assets/templates/post.png')
-  }
-]
+export default []
