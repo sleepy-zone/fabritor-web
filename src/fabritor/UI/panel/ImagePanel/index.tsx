6647a9a570d27b878a722c1f86831f32d80dcd0a
bobo <micro_sobby@outlook.com>
2023-12-19 21:32:14 +0800

    feat(image): pixabay

--- src/fabritor/UI/panel/ImagePanel/index.tsx
+++ src/fabritor/UI/panel/ImagePanel/index.tsx
@@ -1,9 +1,17 @@
 import LocalImagePanel from './LocalImagePanel';
 import { createSvg, createImage } from '@/editor/image';
 import RemoteImagePanel from './RemoteImagePanel';
-import { Flex } from 'antd';
+import { Button, Flex } from 'antd';
+import Title from '@/fabritor/components/Title';
+import { useEffect, useState } from 'react';
+import FallList from '@/fabritor/components/FallList';
+import { fetchPhotos } from './pixabay';
 
 export default function ImagePanel () {
+  const [photos, setPhotos] = useState([]);
+  const [illustrations, setIllustrations] = useState([]);
+  const [vectors, setVectors] = useState([]);
+
   const addImage = async (options) => {
     await createImage({
       imageSource: options.img || options.url
@@ -14,12 +22,41 @@ export default function ImagePanel () {
     await createSvg(options);
   }
 
+  const addPixabay = (item) => {
+    createImage({
+      imageSource: item.cover
+    });
+  }
+
+  const renderPixabayButton = (type) => {
+    return (
+      <Button
+        type="link"
+        href={`https://pixabay.com/zh/${type}s/`}
+        target="_blank">
+        pixabay
+      </Button>
+    )
+  }
+
+  useEffect(() => {
+    fetchPhotos('photo').then(setPhotos);
+    fetchPhotos('illustration').then(setIllustrations);
+    fetchPhotos('vector').then(setVectors);
+  }, []);
+
   return (
     <div className="fabritor-panel-wrapper">
       <Flex gap={10} justify="space-around">
         <LocalImagePanel addImage={addImage} addSvg={addSvg} />
         <RemoteImagePanel addImage={addImage} />
       </Flex>
+      <Title>来自{renderPixabayButton('vector')}的矢量图</Title>
+      <FallList list={vectors} type="image" itemClick={addPixabay} />
+      <Title>来自{renderPixabayButton('photo')}的图片</Title>
+      <FallList list={photos} type="image" itemClick={addPixabay} />
+      <Title>来自{renderPixabayButton('illustration')}的插画</Title>
+      <FallList list={illustrations} type="image" itemClick={addPixabay} />
     </div>
   )
 }
--- src/fabritor/UI/panel/TemplatePanel/index.tsx
+++ src/fabritor/UI/panel/TemplatePanel/index.tsx
@@ -1,17 +1,17 @@
 import { useRef, useContext, useEffect, useState } from 'react';
-import { Button, Flex, Card } from 'antd';
+import { Button } from 'antd';
 import LocalFileSelector from '@/fabritor/components/LocalFileSelector';
 import { getGlobalEditor } from '@/utils/global';
 import { GloablStateContext } from '@/context';
 import Title from '@/fabritor/components/Title';
 import getTemplateList, { getTemplate } from './template-list';
+import FallList from '@/fabritor/components/FallList';
 
 export default function TemplatePanel (props) {
   const { onLoadTpl } = props;
   const localFileSelectorRef = useRef<any>(null);
   const { isReady, setReady } = useContext(GloablStateContext);
-  const [leftList, setLeftList] = useState([]);
-  const [rightList, setRightList] = useState([]);
+  const [tList, setTList] = useState([]);
 
   const startLoad = () => {
     localFileSelectorRef.current?.start?.();
@@ -46,9 +46,7 @@ export default function TemplatePanel (props) {
 
   useEffect(() => {
     getTemplateList().then((res) => {
-      const length = Math.ceil(res.length / 2);
-      setLeftList(res.slice(0, length));
-      setRightList(res.slice(length));
+      setTList(res);
     }).catch(() => {});
   }, []);
 
@@ -58,46 +56,7 @@ export default function TemplatePanel (props) {
         加载本地模板
       </Button>
       <Title>选择一个模板开始</Title>
-      <Flex gap={10} wrap="wrap" justify="space-around">
-        <Flex vertical gap={10}>
-        {
-          leftList.map(item => (
-            <Card
-              hoverable
-              style={{ width: 140 }}
-              key={item.url}
-              cover={
-                <img
-                  src={item.cover} 
-                />
-              }
-              onClick={() => { handleLoadTemplate(item) }}
-            >
-              <Card.Meta description={item.title} />
-            </Card>
-          ))
-        }
-        </Flex>
-        <Flex vertical gap={10}>
-        {
-          rightList.map(item => (
-            <Card
-              hoverable
-              style={{ width: 140 }}
-              key={item.url}
-              cover={
-                <img
-                  src={item.cover} 
-                />
-              }
-              onClick={() => { handleLoadTemplate(item) }}
-            >
-              <Card.Meta description={item.title} />
-            </Card>
-          ))
-        }
-        </Flex>
-      </Flex>
+      <FallList list={tList} itemClick={handleLoadTemplate} />
       <LocalFileSelector ref={localFileSelectorRef} accept="application/json" onChange={handleFileChange} />
     </div>
   )
