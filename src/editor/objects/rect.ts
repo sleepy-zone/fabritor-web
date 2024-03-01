17693c5770a7dd8223b53afbd46df52b99528c28
Sleepy Zone <micro_sobby@outlook.com>
2024-03-01 06:07:26 +0000

    chore: 3.0 version

--- src/editor/objects/rect.ts
+++ src/editor/objects/rect.ts
@@ -3,7 +3,7 @@ import { uuid } from '@/utils';
 import { getGlobalEditor } from '@/utils/global';
 
 export default function createRect (options) {
-  const { width = 200, height = 200, left, top, ...rest } = options || {};
+  const { width = 200, height = 200, ...rest } = options || {};
   const editor = getGlobalEditor();
   const { canvas } = editor;
   const rect = new fabric.Rect({
