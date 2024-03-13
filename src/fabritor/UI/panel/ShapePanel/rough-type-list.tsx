ecd773b0b07418f79fee494c056e337b0bd0ccfb
Sleepy Zone <micro_sobby@outlook.com>
2024-03-13 07:39:25 +0000

    feat: add rough

--- src/fabritor/UI/panel/ShapePanel/rough-type-list.tsx
+++ src/fabritor/UI/panel/ShapePanel/rough-type-list.tsx
@@ -1,5 +1,9 @@
 import roughRectIcon from '@/assets/rough/rect.png';
 import roughLineIcon from '@/assets/rough/line.png';
+import roughCircleIcon from '@/assets/rough/circle.png';
+import roughEllipseIcon from '@/assets/rough/ellipse.png';
+import roughRightAngleIcon from '@/assets/rough/right-angle.png';
+import roughDiamondIcon from '@/assets/rough/diamond.png';
 
 const DEFAULT_ROUGH_CONFIG = {
   hachureGap: 16,
@@ -21,6 +25,42 @@ export default [
   {
     key: 'rough-rect',
     elem: roughRectIcon,
+    options: {
+      fill: '#F6C445',
+      stroke: '#EC6A52',
+      ...DEFAULT_ROUGH_CONFIG
+    }
+  },
+  {
+    key: 'rough-circle',
+    elem: roughCircleIcon,
+    options: {
+      fill: '#F6C445',
+      stroke: '#EC6A52',
+      ...DEFAULT_ROUGH_CONFIG
+    }
+  },
+  {
+    key: 'rough-ellipse',
+    elem: roughEllipseIcon,
+    options: {
+      fill: '#F6C445',
+      stroke: '#EC6A52',
+      ...DEFAULT_ROUGH_CONFIG
+    }
+  },
+  {
+    key: 'rough-right-angle',
+    elem: roughRightAngleIcon,
+    options: {
+      fill: '#F6C445',
+      stroke: '#EC6A52',
+      ...DEFAULT_ROUGH_CONFIG
+    }
+  },
+  {
+    key: 'rough-diamond',
+    elem: roughDiamondIcon,
     options: {
       fill: '#F6C445',
       stroke: '#EC6A52',
