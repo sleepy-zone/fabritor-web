ded474c781b19d5752467db20346b45853482127
Sleepy Zone <micro_sobby@outlook.com>
2024-05-23 06:47:21 +0000

    wip(i18n): app panel

--- src/fabritor/UI/panel/AppPanel/Emoji.tsx
+++ src/fabritor/UI/panel/AppPanel/Emoji.tsx
@@ -1,14 +1,17 @@
 import { fabric } from 'fabric';
+import { useLocale } from 'ice';
 import AppSubPanel from './AppSubPanel';
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 import { createTextbox } from '@/editor/objects/textbox';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GloablStateContext } from '@/context';
 
 export default function EmojiPanel (props) {
   const { back } = props;
   const { editor } = useContext(GloablStateContext);
+  const { locale } = useLocale();
+  const [emojiLocale, setEmojiLocale] = useState('en');
 
   const handleEmojiSelect = async (emoji) => {
     const object = editor.canvas.getActiveObject() as fabric.Textbox;
@@ -25,13 +28,25 @@ export default function EmojiPanel (props) {
     }
   }
 
+  useEffect(() => {
+    if (locale?.indexOf('en') === 0) {
+      setEmojiLocale('en');
+    } else if (locale?.indexOf('zh') === 0) {
+      setEmojiLocale('zh');
+    } else {
+      setEmojiLocale(locale || 'en');
+    }
+  }, [locale]);
+
   return (
     <AppSubPanel title="Emoji" back={back}>
       <Picker
         data={data}
         perLine={8}
         set="native"
-        locale="zh"
+        locale={emojiLocale}
+        emojiButtonSize={30}
+        emojiSize={22}
         onEmojiSelect={handleEmojiSelect}
       />
     </AppSubPanel>
